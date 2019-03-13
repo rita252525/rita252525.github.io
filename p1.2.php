4dfe6f9a13f1622a68e372dd1dce43aac1cf454d
rita252525 <39363856+rita252525@users.noreply.github.com>
2019-03-13 23:08:00 +0500

    Add files via upload

---
+++ p1.2.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel='stylesheet' href='page1.css' type='text/css' media='all' />
+</head>
+<title>Website of Bogdanova Rimma</title>
+<meta name=”description” content=”Works of Bogdanova Rimma. Paintings, art dolls, Website, SVG, animation, grahpick ” />
+<meta name=”robots” content=”noodp” />
+<meta name=”keywords” content=”Bogdanova Rimma, Website, SVG, art dolls, animations, grahpick, art work, paintings, 
+order a site, order animation, handmade dolls, landscape, watercolor, acrylic, watercolor painting, 
+portraits, order a portrait, birthday gifts />
+
+<body>
+<div class="header"  ><FONT FACE="Times New Roman"> 
+<right> <table  width="90%"><tr><td width='90%' align="center"rowspan="2" >
+ <h1 style="padding-left:50%"> Works of Bogdanova Rimma</h1>
+ <p1 ><i style="padding-left:69%"> Paintings</p1></br><p1 ><i style="padding-left:70%">Grafic </p1></br>
+ <p1><i style="padding-left:45%">Art dolls  </p1></br> <p1>SVG</i> </p1>
+ 
+ 
+ </td>
+<td width="10%" rowspan="2" ><img src="Peacock.jpg"  alt="Girl with Peacock. Watercolor 
+on paper "  style="width:17em;"
+ align="left"></tr></td>
+
+  
+ 
+ </tr> </table></right>
+</div>
+
+<ul>
+  <li><a href="http://testsite.local/script/site1/p1.2.php">About me</a></li>
+  <li><a href="http://testsite.local/script/site1/p2.2.php">Paintings</a></li>
+  <li><a href="http://testsite.local/script/site1/p3.2.php">Grafic</a></li>
+  <li><a href="http://testsite.local/script/site1/p4.2.php">Art dolls</a></li>
+  <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">Animation</a>
+    <div class="dropdown-content">
+      <a href="#">Animation Website for Kids</a>
+      <a href="http://testsite.local/script/site1/p5.2.php">SVG animation</a>
+   <li><a href="http://testsite.local/script/site1/p6.2.php">Shop</a></li> 
+     <li><a href="http://testsite.local/script/site1/p7.2.php">Contact</a></li>
+      </div>
+  </li>
+</ul>
+<div class="container1" ><div id="header1">	
+<h2>My name is Rimma Bogdanova</h2>
+      
+<div class="fakeimg1" ">
+<img src="45.jpg"  style="width:53%" align="center">
+<img src="Dawn.jpg"  style="width:45.95%" alt="Dawn.Watercolor.Bogdanova" align="center">
+
+</div>
+<font size=4><h3  align="center">For your viewing, I present my works.</br>
+I am a participant of several exhibitions. </br>I live in Russia at the moment. </h3> </p>
+<div class="fakeimg4" >
+&nbsp<img src="Watercolor.jpg"  style="width:43.4%" alt="Watercolor.Rimma Bogdanova" align="center" >
+&nbsp&nbsp&nbsp
+<img src="Model.jpg"  style="width:43.4%" alt="Model.Watercolor. Rimma Bogdanova" align="center"> </br>
+ <font size=4><p class="main" align="justify">On the site I posted some of my work - it is watercolor,
+graphics created using computer programs, handmade dolls, animated sites created using SVG, PHP, JS, HTML, CSS.
+At the moment I improve my programming skills. I like to combine my creative skills with programming.
+</div></div></div></div></div>
+
+<div class="container2" ><div class="fakeimg2" ">
+<h2><center>STILL IN CHILDHOOD I LIKED TO PAINT</center></h2>
+<font size=4><p class="main" align="justify">&nbsp &nbsp &nbsp &nbsp 
+In childhood I had a lot of hobbies. The creative process really fascinated me. I liked to paint, write stories,
+ composed music. After some time, I again took up painting, made several handmade dolls and wrote several short stories.
+</br> &nbsp &nbsp &nbsp &nbsp
+When bright images come to life on paper and the white sheet turns into a separate frame from the movie of someone's 
+life - I understand that this is my inspiration. Having embodied on paper someone's image, I get a new impetus to 
+new images that seek to incarnate in the real world, conveying messages, and sometimes encouraging doing something, 
+and maybe I just could see a part of someone's life.Thoughtful images on portraits reveal the meaning of their life,
+ I observe their views on this world and their eyes reveal their secret for a short moment. My characters are people
+from the real world and characters invented by me, created from several images and my ideas about people.
+
+&nbsp &nbsp &nbsp &nbsp
+
+  </p>
+ &nbsp<img src="Girl.jpg" style="width:46.1%" alt="Girl On Swing. Akril. Rimma Bogdanova" align="center"> 
+ <img src="rosest.jpg" style="width:51.9%"  align="center"> 
+&nbsp</br>
+&nbsp
+</div></div>
+
+<div class="container3" ><div class="fakeimg3" ">
+<marquee direction="down" behavior="scroll"   LOOP=INFINITY    width="100%" 
+ height="99%"   > <CENTER>WATERCOLOR</br> OF BOGDANOVA RIMMA</CENTER>
+<img src="Model.jpg" style="width:100%" alt="Watercolor.Bogdanova" align="center">&nbsp &nbsp &nbsp 
+<img src="Dawn.jpg" style="width:100%" alt="Dawn.Watercolor.Bogdanova" align="center">&nbsp &nbsp &nbsp
+<img src="Spring.jpg" style="width:100%" alt="Spring mood,watercolor" align="center">&nbsp &nbsp &nbsp
+<img src="rosest.jpg" style="width:100%" alt="Tea roses,watercolor" align="center">&nbsp &nbsp &nbsp
+<img src="23.jpg" style="width:100%"  align="center">&nbsp &nbsp &nbsp
+<img src="Leonardo.jpg" style="width:100%" alt="Leonardo.Watercolor.Portret" align="center">&nbsp &nbsp &nbsp
+<img src="25.jpg" style="width:100%"  align="center">&nbsp &nbsp &nbsp
+<img src="27.jpg" style="width:100%" align="center">&nbsp &nbsp &nbsp
+<img src="serval.jpg" style="width:100%" alt="serval,watercolor,Bogdanova" align="center">&nbsp &nbsp &nbsp
+<img src="29.jpg" style="width:100%" align="center">&nbsp &nbsp &nbsp
+<img src="6.jpg" style="width:100%" align="center">&nbsp &nbsp &nbsp
+<img src="Caracal.jpg" style="width:100%" alt="My cat Caracal,watercolor.Bogdanova" align="center">&nbsp &nbsp &nbsp
+<img src="peacock.jpg" style="width:100%" alt="Girl with peacock" align="center">&nbsp &nbsp &nbsp
+</marquee>
+
+</div></div>
+
+
